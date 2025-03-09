aed7667321b15c8be767cfa085b634eb7c541b45
k4karim <k4karim27@gmail.com>
2025-03-09 15:36:04 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import render_template, request, redirect, Flask
+import os
+import csv
 
 app = Flask(__name__)
+@app.route('/', methods=["GET", "POST"])
+def index():
+    data = []
+    
+    if request.method == 'POST':
+        if request.files:
+            uploaded_file = request.files['filename'] # This line uses the same variable and worked fine
+            filepath = os.path.join(app.config['FILE_UPLOADS'], uploaded_file.filename)
+            uploaded_file.save(filepath)
+            
+            with open(filepath) as file:
+                csv_file = csv.reader(file)
+                for row in csv_file:
+                    data.append(row)
+            
+            return render_template('index.html', data=data)
+    
+    return render_template('index.html')
 
-@app.route('/')
-def home():
-    return 'Hello, sss!'
 
-@app.route('/about')
-def about():
-    return 'About'
+
+app.config['FILE_UPLOADS'] = "uploadds"
+if __name__ == "__main__":
+    app.run()
